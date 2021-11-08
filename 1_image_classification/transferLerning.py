a5258e91fbacc48147d94b6262fd3cc6d598a651
Thanyu <hanyutaisei77@gmail.com>
2021-11-08 14:49:31 -0600

    Added someld

--- 1_image_classification/transferLerning.py
+++ 1_image_classification/transferLerning.py
@@ -7,16 +7,17 @@ from PIL import Image
 from numpy.lib.type_check import imag
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-
+import pprint as pr
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 from torchvision import models, transforms
 
-torch.manual_seed(1234)
-np.random.seed(1234)
-random.seed(1234)
+temnum = 1234
+torch.manual_seed(temnum)
+np.random.seed(temnum)
+random.seed(temnum)
 
 
 class ImageTransform():
@@ -51,8 +52,8 @@ class ImageTransform():
 image_file_path = './pytorch_advanced/1_image_classification/data/goldenretriever-3724972_640.jpg'
 img = Image.open(image_file_path)
 
-plt.imshow(img)
-plt.show()
+# plt.imshow(img)
+# plt.show()
 
 size = 224
 mean = (0.485, 0.456, 0.406)
@@ -63,5 +64,133 @@ img_transformed = transform(img, phase='train')
 
 img_transformed = img_transformed.numpy().transpose((1, 2, 0))
 img_transformed = np.clip(img_transformed, 0, 1)
-plt.imshow(img_transformed)
-plt.show()
+""" plt.imshow(img_transformed)
+plt.show() """
+
+
+def make_datapth_list(phase="train"):
+
+    rootpath = "./pytorch_advanced/1_image_classification/data/hymenoptera_data/"
+    target_path = osp.join(rootpath+phase+'/**/*.jpg')
+    print(target_path)
+
+    path_list = []
+
+    for path in glob.glob(target_path):
+        path_list.append(path)
+
+    return path_list
+
+
+train_list = make_datapth_list(phase="train")
+val_list = make_datapth_list(phase="val")
+
+# pr.pprint(train_list)
+
+
+"""class HymenopteraDataset(data.Dataset):
+    def __init__(self, file_list, transform=None, phase='train'):
+        self.file_list = file_list
+        self.file_list = transform
+        self.phase = phase
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        img_path = self.file_list[index]
+        img = Image.open(img_path)
+
+        img_transformed = self.transform(
+            img, self.phase
+        )
+
+        if self.phase == "train":
+            label = img_path[30:34]
+        elif self.phase == "val":
+            label = img_path[28:32]
+
+        if label == "ant":
+            label = 0
+        elif label == "bees":
+            label = 1
+
+            return img_transformed, label
+
+
+train_dataset = HymenopteraDataset(
+    file_list=train_list, transform=ImageTransform(size, mean, std), phase='train')
+
+val_dataset = HymenopteraDataset(
+    file_list=val_list, transform=ImageTransform(size, mean, std), phase='val')
+
+index = 0
+pr.pprint(train_dataset.__getitem__(index)[0].size())
+pr.pprint(train_dataset.__getitem__(index)[1])
+"""
+
+# アリとハチの画像のDatasetを作成する
+
+
+class HymenopteraDataset(data.Dataset):
+    """
+    アリとハチの画像のDatasetクラス。PyTorchのDatasetクラスを継承。
+
+    Attributes
+    ----------
+    file_list : リスト
+        画像のパスを格納したリスト
+    transform : object
+        前処理クラスのインスタンス
+    phase : 'train' or 'val'
+        訓練か検証かを設定
+    """
+
+    def __init__(self, file_list, transform=None, phase='train'):
+        self.file_list = file_list  # ファイルパスのリスト
+        self.transform = transform  # 前処理クラスのインスタンス
+        self.phase = phase  # train or valの指定
+
+    def __len__(self):
+        '''画像の枚数を返す'''
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        '''
+        前処理をした画像のTensor形式のデータとラベルを取得
+        '''
+
+        # index番目の画像をロード
+        img_path = self.file_list[index]
+        img = Image.open(img_path)  # [高さ][幅][色RGB]
+
+        # 画像の前処理を実施
+        img_transformed = self.transform(
+            img, self.phase)  # torch.Size([3, 224, 224])
+
+        # 画像のラベルをファイル名から抜き出す
+        if self.phase == "train":
+            label = img_path[30:34]
+        elif self.phase == "val":
+            label = img_path[28:32]
+
+        # ラベルを数値に変更する
+        if label == "ants":
+            label = 0
+        elif label == "bees":
+            label = 1
+
+        return img_transformed, label
+
+
+# 実行
+train_dataset = HymenopteraDataset(
+    file_list=train_list, transform=ImageTransform(size, mean, std), phase='train')
+
+val_dataset = HymenopteraDataset(
+    file_list=val_list, transform=ImageTransform(size, mean, std), phase='val')
+
+# 動作確認
+index = 0
+print(train_dataset.__getitem__(index)[0].size())
+print(train_dataset.__getitem__(index)[1])
