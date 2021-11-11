5ef07e2b30ee73642ccb631aac16c673999ee08e
Thanyu <hanyutaisei77@gmail.com>
2021-11-11 16:00:46 -0600

    done

--- 1_image_classification/transferLerning.py
+++ 1_image_classification/transferLerning.py
@@ -4,21 +4,26 @@ import random
 import numpy as np
 import json
 from PIL import Image
-from numpy.lib.type_check import imag
-from tqdm import tqdm
+from tqdm import  tqdm
 import matplotlib.pyplot as plt
 import pprint as pr
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 from torchvision import models, transforms
 
-temnum = 1234
-torch.manual_seed(temnum)
-np.random.seed(temnum)
-random.seed(temnum)
+rndnum = 1234
+torch.manual_seed(rndnum)
+np.random.seed(rndnum)
+random.seed(rndnum)
 
+
+
+size = 224
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
 
 class ImageTransform():
     def __init__(self, resize, mean, std):
@@ -48,27 +53,7 @@ class ImageTransform():
         """
         return self.data_transform[phase](img)
 
-
-image_file_path = './pytorch_advanced/1_image_classification/data/goldenretriever-3724972_640.jpg'
-img = Image.open(image_file_path)
-
-# plt.imshow(img)
-# plt.show()
-
-size = 224
-mean = (0.485, 0.456, 0.406)
-std = (0.229, 0.224, 0.225)
-
-transform = ImageTransform(size, mean, std)
-img_transformed = transform(img, phase='train')
-
-img_transformed = img_transformed.numpy().transpose((1, 2, 0))
-img_transformed = np.clip(img_transformed, 0, 1)
-""" plt.imshow(img_transformed)
-plt.show() """
-
-
-def make_datapth_list(phase="train"):
+def make_datapath_list(phase="train"):
 
     rootpath = "./pytorch_advanced/1_image_classification/data/hymenoptera_data/"
     target_path = osp.join(rootpath+phase+'/**/*.jpg')
@@ -81,118 +66,159 @@ def make_datapth_list(phase="train"):
 
     return path_list
 
+train_list = make_datapath_list(phase="train")
+val_list = make_datapath_list(phase="val")
 
-train_list = make_datapth_list(phase="train")
-val_list = make_datapth_list(phase="val")
+class HymenopteraDataset(data.Dataset):
 
-# pr.pprint(train_list)
-
-
-"""class HymenopteraDataset(data.Dataset):
-    def __init__(self, file_list, transform=None, phase='train'):
+    def __init__(self, file_list, transform=None, phase="train"):
         self.file_list = file_list
-        self.file_list = transform
+        self.transform = transform
         self.phase = phase
 
     def __len__(self):
         return len(self.file_list)
 
     def __getitem__(self, index):
+
         img_path = self.file_list[index]
         img = Image.open(img_path)
 
         img_transformed = self.transform(
             img, self.phase
         )
-
         if self.phase == "train":
-            label = img_path[30:34]
+            wordPosition = int(img_path.find("train"))+6
+            label = img_path[wordPosition:wordPosition+4]
         elif self.phase == "val":
-            label = img_path[28:32]
-
-        if label == "ant":
-            label = 0
-        elif label == "bees":
-            label = 1
-
-            return img_transformed, label
+            wordPosition = int(img_path.find("val"))+4
+            label = img_path[wordPosition:wordPosition+5]
 
 
-train_dataset = HymenopteraDataset(
-    file_list=train_list, transform=ImageTransform(size, mean, std), phase='train')
-
-val_dataset = HymenopteraDataset(
-    file_list=val_list, transform=ImageTransform(size, mean, std), phase='val')
-
-index = 0
-pr.pprint(train_dataset.__getitem__(index)[0].size())
-pr.pprint(train_dataset.__getitem__(index)[1])
-"""
-
-# アリとハチの画像のDatasetを作成する
-
-
-class HymenopteraDataset(data.Dataset):
-    """
-    アリとハチの画像のDatasetクラス。PyTorchのDatasetクラスを継承。
-
-    Attributes
-    ----------
-    file_list : リスト
-        画像のパスを格納したリスト
-    transform : object
-        前処理クラスのインスタンス
-    phase : 'train' or 'val'
-        訓練か検証かを設定
-    """
-
-    def __init__(self, file_list, transform=None, phase='train'):
-        self.file_list = file_list  # ファイルパスのリスト
-        self.transform = transform  # 前処理クラスのインスタンス
-        self.phase = phase  # train or valの指定
-
-    def __len__(self):
-        '''画像の枚数を返す'''
-        return len(self.file_list)
-
-    def __getitem__(self, index):
-        '''
-        前処理をした画像のTensor形式のデータとラベルを取得
-        '''
-
-        # index番目の画像をロード
-        img_path = self.file_list[index]
-        img = Image.open(img_path)  # [高さ][幅][色RGB]
-
-        # 画像の前処理を実施
-        img_transformed = self.transform(
-            img, self.phase)  # torch.Size([3, 224, 224])
-
-        # 画像のラベルをファイル名から抜き出す
-        if self.phase == "train":
-            label = img_path[30:34]
-        elif self.phase == "val":
-            label = img_path[28:32]
-
-        # ラベルを数値に変更する
         if label == "ants":
             label = 0
+
         elif label == "bees":
             label = 1
 
         return img_transformed, label
 
 
-# 実行
 train_dataset = HymenopteraDataset(
     file_list=train_list, transform=ImageTransform(size, mean, std), phase='train')
 
 val_dataset = HymenopteraDataset(
     file_list=val_list, transform=ImageTransform(size, mean, std), phase='val')
 
-# 動作確認
-index = 0
-print(train_dataset.__getitem__(index)[0].size())
-print(train_dataset.__getitem__(index)[1])
+batch_size = 32
 
-#contribution Check
+# DataLoaderを作成
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True)
+
+val_dataloader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=batch_size, shuffle=False)
+
+# 辞書型変数にまとめる
+dataloaders_dict = {"train": train_dataloader, "val": val_dataloader}
+
+# 動作確認
+batch_iterator = iter(dataloaders_dict["train"])  # イテレータに変換
+inputs, labels = next(
+    batch_iterator)  # 1番目の要素を取り出す
+print(inputs.size())
+print(labels)
+
+# 学習済みのVGG-16モデルをロード
+# VGG-16モデルのインスタンスを生成
+use_pretrained = True  # 学習済みのパラメータを使用
+net = models.vgg16(pretrained=use_pretrained)
+
+# VGG16の最後の出力層の出力ユニットをアリとハチの2つに付け替える
+net.classifier[6] = nn.Linear(in_features=4096, out_features=2)
+
+# 訓練モードに設定
+net.train()
+
+print('ネットワーク設定完了：学習済みの重みをロードし、訓練モードに設定しました')
+
+criterion = nn.CrossEntropyLoss()
+# 転移学習で学習させるパラメータを、変数params_to_updateに格納する
+params_to_update = []
+
+# 学習させるパラメータ名
+update_param_names = ["classifier.6.weight", "classifier.6.bias"]
+
+# 学習させるパラメータ以外は勾配計算をなくし、変化しないように設定
+for name, param in net.named_parameters():
+    if name in update_param_names:
+        param.requires_grad = True
+        params_to_update.append(param)
+        print(name)
+    else:
+        param.requires_grad = False
+
+# params_to_updateの中身を確認
+print("-----------")
+print(params_to_update)
+
+optimizer = optim.SGD(params=params_to_update, lr=0.001, momentum=0.9)
+
+# モデルを学習させる関数を作成
+
+
+def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs):
+
+    # epochのループ
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+        print('-------------')
+
+        # epochごとの学習と検証のループ
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                net.train()  # モデルを訓練モードに
+            else:
+                net.eval()   # モデルを検証モードに
+
+            epoch_loss = 0.0  # epochの損失和
+            epoch_corrects = 0  # epochの正解数
+
+            # 未学習時の検証性能を確かめるため、epoch=0の訓練は省略
+            if (epoch == 0) and (phase == 'train'):
+                continue
+
+            # データローダーからミニバッチを取り出すループ
+            for inputs, labels in tqdm(dataloaders_dict[phase]):
+
+                # optimizerを初期化
+                optimizer.zero_grad()
+
+                # 順伝搬（forward）計算
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = net(inputs)
+                    loss = criterion(outputs, labels)  # 損失を計算
+                    _, preds = torch.max(outputs, 1)  # ラベルを予測
+
+
+                    # 訓練時はバックプロパゲーション
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                    # イタレーション結果の計算
+                    # lossの合計を更新
+                    epoch_loss += loss.item() * inputs.size(0)
+                    # 正解数の合計を更新
+                    epoch_corrects += torch.sum(preds == labels.data)
+
+            # epochごとのlossと正解率を表示
+            epoch_loss = epoch_loss / len(dataloaders_dict[phase].dataset)
+            epoch_acc = epoch_corrects.double(
+            ) / len(dataloaders_dict[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+num_epochs=2
+train_model(net, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs)
