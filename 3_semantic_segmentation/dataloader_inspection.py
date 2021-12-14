1ca8b1ce3d7abb7a8dde679021e65b0568375e57
Thanyu <hanyutaisei77@gmail.com>
2021-12-13 23:43:02 -0600

    Dec_13

--- 3_semantic_segmentation/dataloader_inspection.py
+++ 3_semantic_segmentation/dataloader_inspection.py
@@ -104,94 +104,73 @@ def lambda_epoch(epoch):
 
 scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_epoch)
 
-# モデルを学習させる関数を作成
-
 
 def train_model(net, dataloaders_dict, criterion, scheduler, optimizer, num_epochs):
 
-    # GPUが使えるかを確認
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("使用デバイス：", device)
 
-    # ネットワークをGPUへ
     net.to(device)
 
-    # ネットワークがある程度固定であれば、高速化させる
     torch.backends.cudnn.benchmark = True
 
-    # 画像の枚数
     num_train_imgs = len(dataloaders_dict["train"].dataset)
     num_val_imgs = len(dataloaders_dict["val"].dataset)
     batch_size = dataloaders_dict["train"].batch_size
 
-    # イテレーションカウンタをセット
     iteration = 1
     logs = []
 
-    # multiple minibatch
     batch_multiplier = 3
 
-    # epochのループ
     for epoch in range(num_epochs):
 
-        # 開始時刻を保存
         t_epoch_start = time.time()
         t_iter_start = time.time()
-        epoch_train_loss = 0.0  # epochの損失和
-        epoch_val_loss = 0.0  # epochの損失和
+        epoch_train_loss = 0.0
+        epoch_val_loss = 0.0
 
         print('-------------')
         print('Epoch {}/{}'.format(epoch+1, num_epochs))
         print('-------------')
 
-        # epochごとの訓練と検証のループ
         for phase in ['train', 'val']:
             if phase == 'train':
-                net.train()  # モデルを訓練モードに
-                scheduler.step()  # 最適化schedulerの更新
+                net.train()
+                scheduler.step()
                 optimizer.zero_grad()
                 print('（train）')
 
             else:
                 if((epoch+1) % 5 == 0):
-                    net.eval()   # モデルを検証モードに
+                    net.eval()
                     print('-------------')
                     print('（val）')
                 else:
-                    # 検証は5回に1回だけ行う
                     continue
 
-            # データローダーからminibatchずつ取り出すループ
-            count = 0  # multiple minibatch
+            count = 0
             for imges, anno_class_imges in dataloaders_dict[phase]:
-                # ミニバッチがサイズが1だと、バッチノーマライゼーションでエラーになるのでさける
-                # issue #186より不要なのでコメントアウト
-                # if imges.size()[0] == 1:
-                #     continue
 
-                # GPUが使えるならGPUにデータを送る
                 imges = imges.to(device)
                 anno_class_imges = anno_class_imges.to(device)
 
 
-                # multiple minibatchでのパラメータの更新
                 if (phase == 'train') and (count == 0):
                     optimizer.step()
                     optimizer.zero_grad()
                     count = batch_multiplier
 
-                # 順伝搬（forward）計算
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = net(imges)
                     loss = criterion(
                         outputs, anno_class_imges.long()) / batch_multiplier
 
-                    # 訓練時はバックプロパゲーション
                     if phase == 'train':
-                        loss.backward()  # 勾配の計算
-                        count -= 1  # multiple minibatch
+                        loss.backward()
+                        count -= 1
 
-                        if (iteration % 10 == 0):  # 10iterに1度、lossを表示
+                        if (iteration % 10 == 0):
                             t_iter_finish = time.time()
                             duration = t_iter_finish - t_iter_start
                             print('イテレーション {} || Loss: {:.4f} || 10iter: {:.4f} sec.'.format(
@@ -201,11 +180,9 @@ def train_model(net, dataloaders_dict, criterion, scheduler, optimizer, num_epoc
                         epoch_train_loss += loss.item() * batch_multiplier
                         iteration += 1
 
-                    # 検証時
                     else:
                         epoch_val_loss += loss.item() * batch_multiplier
 
-        # epochのphaseごとのlossと正解率
         t_epoch_finish = time.time()
         print('-------------')
         print('epoch {} || Epoch_TRAIN_Loss:{:.4f} ||Epoch_VAL_Loss:{:.4f}'.format(
@@ -213,111 +190,16 @@ def train_model(net, dataloaders_dict, criterion, scheduler, optimizer, num_epoc
         print('timer:  {:.4f} sec.'.format(t_epoch_finish - t_epoch_start))
         t_epoch_start = time.time()
 
-        # ログを保存
         log_epoch = {'epoch': epoch+1, 'train_loss': epoch_train_loss /
                      num_train_imgs, 'val_loss': epoch_val_loss/num_val_imgs}
         logs.append(log_epoch)
         df = pd.DataFrame(logs)
         df.to_csv("log_output.csv")
 
-    # 最後のネットワークを保存する
     torch.save(net.state_dict(), 'weights/pspnet50_' +
                str(epoch+1) + '.pth')
 
-# def train_model(net, dataloaders_dict, criterion, scheduler, optimizer, num_epochs):
-#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#     print("Device: ", device)
 
-#     net.to(device)
-
-#     torch.backends.cudnn.benchmark = True
-
-#     num_train_imgs = len(dataloaders_dict["train"].dataset)
-#     num_val_imgs = len(dataloaders_dict["val"].dataset)
-#     batch_size = dataloaders_dict["train"].batch_size
-
-#     iteration = 1
-#     logs = []
-
-#     batch_multiplier = 3
-
-#     for epoch in range(num_epochs):
-
-#         t_epoch_start = time.time()
-#         t_iter_start = time.time()
-#         epoch_train_loss = 0.0
-#         epoch_val_loss = 0.0
-
-#         print("--------")
-#         print("Epoch {}/{}".format(epoch+1, num_epochs))
-#         print("--------")
-
-#         for phase in ["train","val"]:
-#             if phase == 'train':
-#                 net.train()
-#                 scheduler.step()
-#                 optimizer.zero_grad()
-#                 print(' (train) ')
-
-#             else:
-#                 if((epoch+1) % 5 == 0):
-#                     net.eval()
-#                     print('--------')
-#                     print(' (val) ')
-
-#                 else:
-#                     continue
-
-#         count = 0
-#         for imgs, anno_class_imgs in dataloaders_dict[phase]:
-
-#             if imgs.size()[0] == 1:
-#                 continue
-
-#             imgs = imgs.to(device)
-#             anno_class_imgs = anno_class_imgs.to(device)
-
-
-#             if (phase == 'train') and (count == 0):
-#                 optimizer.step()
-#                 optimizer.zero_grad()
-#                 count = batch_multiplier
-
-#             with torch.set_grad_enabled(phase == 'train'):
-#                 outputs = net(imgs)
-#                 loss = criterion(outputs, anno_class_imgs.long()) / batch_multiplier
-
-#                 if phase == 'train':
-#                     loss.backward()
-#                     count -= 1
-
-#                     if (iteration % 10 == 0):
-#                         t_iter_finish = time.time()
-#                         duration = t_iter_finish - t_iter_start
-#                         print('Iteration {} || Loss: {:.4f} || 10iter: iteration: {:.4f} sec.'.format(iteration, loss.item()/batch_size*batch_multiplier, duration))
-
-#                         t_iter_start = time.time()
-
-#                         epoch_train_loss += loss.item() * batch_multiplier
-#                         iteration += 1
-
-#                     else:
-#                         epoch_val_loss += loss.item() * batch_multiplier
-#            t_epoch_finish = time.time()
-#                         print('-------')
-#                         print('epoch {} || Epoch_TRAIN_Loss:{:.4f} || Epoch_VAL_Loss:{:.4f}'.format(
-#                             epoch+1, epoch_train_loss/num_train_imgs, epoch_val_loss/num_val_imgs
-#                         ))
-#                         print('timer:  {:.4f} sec.'.format(t_epoch_finish - t_epoch_start))
-#                         t_epoch_start = time.time()
-
-#                         log_epoch = {'epoch':epoch+1, 'train_loss': epoch_train_loss / num_train_imgs, 'val_loss': epoch_val_loss / num_val_imgs}
-
-#                         logs.append(log_epoch)
-#                         df = pd.DataFrame(logs)
-#                         df.to_csv("log_output.csv")
-
-#                     torch.save(net.state_dict(), 'weights/pspnet50_' + str(epoch+1) + '.pth')
 
 num_epochs = 1
 train_model(net, dataloaders_dict, criterion, scheduler, optimizer, num_epochs=num_epochs)
